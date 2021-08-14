b14cd41ffa4aeb0834569c4c00c1b0dc94868b75
kaflemahesh <mahesh.kafle@smarsh.com>
2021-08-14 12:30:02 +0545

    Added primitive Data Types and String Class

---
+++ src/DataTypes.java
@@ -0,0 +1,31 @@
+public class DataTypes {
+
+        public static void main(String[] args) {
+            //
+            // 1. int
+            int i = 1;
+            i = 2;
+            // int i = 0;
+            System.out.println(i);
+
+            // 2. double
+            double k = 12.33D;
+            double l = 14.55D;
+            double m = 15.55D;
+            double n = 100; // interpreted as 100.0
+            System.out.println(n);
+
+            //char holds single character
+            char a = 's';
+            boolean a1 = true;
+            boolean b1 = false;
+            System.out.println(b1);
+
+            // String is not a datatype but a class. We can assign multiple character with String class
+            String s = "This is multiline string value";
+            String t = "12.44";
+            System.out.println(s);
+        }
+}
+
+
