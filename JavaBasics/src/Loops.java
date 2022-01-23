424fbd1b96e101f360665b8f1459624ebc95d8c4
Mahesh Kafle <kafledarkhorse@gmail.com>
2022-01-23 10:41:03 +0400

    Minor Changes

--- JavaBasics/src/Loops.java
+++ JavaBasics/src/Loops.java
@@ -13,7 +13,7 @@ public class Loops {
 
         while(i<=10){ //conditional
             System.out.println("i: "+i);
-            i++; //incremental/decremental
+            i++; //incremental
         }
 
         System.out.println("************");
@@ -22,21 +22,21 @@ public class Loops {
 
         while(a>=0){ //conditional
             System.out.println("a: "+a);
-            a--; //incremental/decremental
+            a--; //decremental
         }
 
         System.out.println("************");
 
         // 2. For Loops
 
-        for(int j=0; j<=10; j++){ //(initialization, conditional, incremental/decremental)
+        for(int j=0; j<=10; j++){ //(initialization, conditional, incremental)
             System.out.println("j: "+j);
         }
 
         System.out.println("************");
 
         //print from 10 to 0
-        for(int k=10; k>=0; k--){ //(initialization, conditional, incremental/decremental)
+        for(int k=10; k>=0; k--){ //(initialization, conditional, decremental)
             System.out.println("k: "+k);
         }
 
