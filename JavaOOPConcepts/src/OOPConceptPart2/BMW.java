b5cd741537efada09d369d2896d691e44bf64ff7
Mahesh Kafle <kafledarkhorse@gmail.com>
2022-01-30 16:18:53 +0400

    Updated JavaOOPs interview Questions

--- JavaOOPConcepts/src/OOPConceptPart2/BMW.java
+++ JavaOOPConcepts/src/OOPConceptPart2/BMW.java
@@ -1,12 +1,16 @@
 package OOPConceptPart2;
 
-public class BMW extends Car{
+public class BMW extends Car{ // This relationship is also called "has-a relationship"
 
-    /* Interview Question: What is Method Overriding?
+    /*
+        Interview Question: What is Method Overriding?
         When same method is present in parent/Super class as well as in child/Sub class with the same name and
         same no. of arguments, then this is called Method Overriding.
     */
-    public void start(){ // Overridden Method
+
+    // This is called Overridden Method in Method Overriding
+    public void start(){
+
         System.out.println("BMW - Start");
     }
 
--- JavaOOPConcepts/src/OOPConceptPart2/TestCar.java
+++ JavaOOPConcepts/src/OOPConceptPart2/TestCar.java
@@ -4,43 +4,70 @@ public class TestCar {
 
   public static void main(String[] args){
 
-      /* What is Polymorphism?
-        Polymorphism means method overriding.
+      /*
+      Interview Questions:
+        Q.  What is Method Overriding?
+        Ans:    When there are methods with same name and same number of arguments inside parent class as well as child class.
+                Then this is known as Method Overriding.
+        Q.  What is Static/ Compile Time Polymorphism?
+        Ans:    In Java, if there are many methods in both the parent and child class with same name and no. of arguments then
+                Java compiler decides which method to execute (preference is given to overridden method - method present in child class)
+                among those methods. This property is known as Static/Compile Time Polymorphism.
       */
-      // Interview Question: Static/Compile Time -> Polymorphism Java decides which method to run during compilation time
+
       BMW b = new BMW();
       b.start();
       b.stop();
       b.refuel();
       b.theftsafety();
-      b.engine(); // Interview Question: BMW class inheriting method from grand parent class i.e Vehicle Class
+      /*
+      Interview Question:
+      Q.    Can I access engine() method using below statement.
+            b.engine();
+      Ans:  Yup. Child class can inherit properties from grand parent class.
+      */
+      b.engine();
 
       System.out.println("*********");
 
-      //
       Car c = new Car();
       c.start();
       c.stop();
       c.refuel();
 
       /*
-      c.theftsafety() : //not allowed
-      Interview Question: Can i access BMW theftsafety() because parents cannot inherit some property from child class
+      Interview Question:
+      Q.    Can I access theftsafety method using below statement.
+            c.theftsafety();
+      Ans:  No because child can inherit all the properties from parent class but parent cannot inherit some properties from child class.
+            theftsafety method is created only inside child class so it is not possible.
       */
 
       System.out.println("*********");
 
-      // Interview Question: What is Dynamic/Run Time Polymorphism
-      // object reference of Parent class referencing to new Child class object
-      Car c1 = new BMW(); // This is also called Top Casting
-      //Imp Interview Question: Can we do Down Casting? in e.g below
-      //BMW b1 =(BMW)new Car(); //No we cannot Down Cast we will get ClassCastException in run time
+      /*
+        Q.  What is Dynamic/Run-Time Polymorphism?
+        Ans:    Child Class Object can be referenced by parent class reference variable. This is known as Dynamic/Run-Time Polymorphism.
+      */
+      // This is also called Top Casting
+      Car c1 = new BMW();
+
+      /*
+        Imp Interview Question:
+        Q.  Can we do Down Casting? in e.g below
+            BMW b1 =(BMW) new Car();
+        Ans:    No we cannot Down Casting because we will get ClassCastException in run time
+      */
+
       c1.start(); // Preference given to child class
       c1.stop();
       c1.refuel();
       /*
-      c.theftsafety() : //Can i access the theftsafety() from child class Ans: It's not allowed
-      Interview Question: Can i access BMW theftsafety() because parents cannot inherit some property from child class
+      Interview Question:
+      Q. Can i access the theftsafety() from child class using parent class reference variable.
+      i.e c1.theftsafety();
+      Ans: No (why?) because only overridden methods(common methods) and parent class methods can be accessed by parent class variable
+      although it is referring to the child class object.
       */
 
 
